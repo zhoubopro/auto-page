1b202ac105ef8c3ae273a6ae3bcf7bc78d47bab9
zhoubo <zhoubo@reignwood.com>
2018-08-02 15:37:45 +0800

    1

---
+++ src/assets/js/AutoPage.js
@@ -0,0 +1,153 @@
+let AutoPage = {
+  tableId: null, // tableId
+  totalHeight: 530,//总的高度
+  divID: null,//全文divID
+  totalRecord: 0,//总记录数
+  selCount: 0,
+  selMoney: 0,
+  dateTime: '',
+
+  init(totalHeight, divID, props) {
+    this.totalHeight = totalHeight;
+    this.divID = divID;
+    this.selCount = props.selCount
+    this.selMoney = props.selMoney
+    this.dateTime = props.dateTime
+    //初始化分页信息
+    this.initPage()
+    //隐藏初始表格
+    this.hiddenContent()
+  },
+
+  initPage() {
+    this.tableId = document.getElementById('hb-auto-page-table')
+    let tbodyList = this.tableId.querySelectorAll('tbody')
+    this.totalRecord = tbodyList.length //初始化总记录数
+
+    let height_tmp = 0 //一页总高度
+    let html_tmp = "";  //临时存储正文
+    let headerHeight = 90
+    let htmlHeader = "<table cellspacing='0' cellpadding='0'>";
+    let htmlFoot = "</table>";
+    let page = 0
+    height_tmp = headerHeight;
+    for(let i = 0; i < this.totalRecord; i++){
+      let tbodyHeight = tbodyList[i].clientHeight;//第i行高度
+      let tbodyHTML = "<tbody>" + tbodyList[i].innerHTML + "</tbody>";//第i行内容
+      height_tmp += tbodyHeight;
+      if(height_tmp < this.totalHeight){
+        if(height_tmp == headerHeight + tbodyHeight){
+          html_tmp += htmlHeader + this.initHeader(page)
+          page++;//页码
+        }
+        html_tmp += tbodyHTML
+        if (i == this.totalRecord - 1) {
+          html_tmp += this.initFooter() + htmlFoot
+        }
+      }
+      else{
+        html_tmp += this.initFooter() + htmlFoot + this.addPageBreak();
+        i--;
+        height_tmp = headerHeight;
+      }
+    }
+
+    document.getElementById(this.divID).innerHTML = html_tmp
+    document.querySelectorAll('.total-page').forEach((object, index)=>{
+      object.innerText = page
+    })
+  },
+  initHeader(page) {
+    let htmlHeader =
+      `<colgroup>
+        <col width="50">
+        <col width="140">
+        <col width="120">
+        <col width="120">
+        <col width="120">
+        <col width="120">
+        <col width="120">
+        <col width="100">
+        <col width="100">
+        <col width="100">
+      </colgroup>
+       <thead style="display:table-header-group">
+        <tr >
+          <td colspan="10" style="height: 40px; border:none;">
+          </td>
+        </tr>
+        <tr>
+          <td
+            colspan="10"
+            align="center"
+            style="height: 40px;font-size: 30px; border: none;"
+          >
+            资金审批单
+          </td>
+        </tr>
+        <tr>
+          <td
+            colspan="6"
+            style="font-size: 16px;height: 40px;border: none;">
+            审批单号：<span class="approval-id"></span>（ 共 ${this.selCount} 条 ）
+            &nbsp;&nbsp; 总金额：${this.selMoney}元
+          </td>
+          <td
+            colspan="4"
+            align="right"
+            style="font-size: 16px;height: 40px;border: none;">
+            &nbsp;&nbsp; 打印时间：${this.dateTime} 
+            &nbsp;&nbsp; ${page + 1}/<span class="total-page"></span> 页
+          </td>
+        </tr>
+      </thead>`
+
+    return htmlHeader
+  },
+  initFooter() {
+    let htmlFooter =
+      `<tfoot>
+        <tr>
+          <td colspan="11" align="left" style="height: 150px;border: none;">
+            &nbsp;
+          </td>
+        </tr>
+        <tr>
+          <td colspan="11" align="left" style="border: none;">
+            董事长签字：___________________________
+          </td>
+        </tr>
+        <tr>
+          <td colspan="11" align="left" style="border: none;">
+            &nbsp;
+          </td>
+        </tr>
+        <tr>
+          <td colspan="11" align="left" style="border: none;text-indent: 38px">
+            日 期：___________________________
+          </td>
+        </tr>
+        <tr>
+          <td colspan="11" align="left" style="border: none;">
+            &nbsp;
+          </td>
+        </tr>
+      </tfoot>`
+
+    return htmlFooter
+  },
+  hiddenContent(){
+    document.getElementById('page-0').style.display = 'none'
+  },
+  ////添加分页符
+  addPageBreak: function () {
+    return "<p style='page-break-before:always;'></p>";
+  },
+  setApprovalID(id){
+    document.querySelectorAll('.approval-id').forEach((object, index)=>{
+      object.innerText = id
+    })
+  }
+}
+
+export default AutoPage
